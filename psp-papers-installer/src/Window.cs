4bd9ade99b6f1c098d4466fe00df2c4605ee16f3
Ethan Jones <ethan@yasfu.net>
2024-04-06 15:12:46 -0400

    fix: dependencies

--- psp-papers-installer/src/Window.cs
+++ psp-papers-installer/src/Window.cs
@@ -1,21 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace psp_papers_installer {
     public partial class Window : Form {
         public Window() {
             InitializeComponent();
-        }
-
-        private void label1_Click(object sender, EventArgs e) {
-            throw new System.NotImplementedException();
         }
     }
 }
