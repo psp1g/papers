70d80d019de3d131e37ba96ae4d1b8c0787941e8
Ethan Jones <ethan@yasfu.net>
2024-04-06 12:47:12 -0400

    feat: .NET Framework 4.8 & happi

--- psp-papers-installer/Finished.Designer.cs
+++ psp-papers-installer/Finished.Designer.cs
@@ -26,10 +26,14 @@ namespace psp_papers_installer {
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Finished));
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.play = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             //
             // label2
@@ -64,19 +68,45 @@ namespace psp_papers_installer {
             //
             // play
             //
-            this.play.Location = new System.Drawing.Point(531, 529);
+            this.play.Location = new System.Drawing.Point(261, 450);
             this.play.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.play.Name = "play";
-            this.play.Size = new System.Drawing.Size(126, 44);
+            this.play.Size = new System.Drawing.Size(141, 44);
             this.play.TabIndex = 7;
             this.play.Text = "Play";
             this.play.UseVisualStyleBackColor = true;
             this.play.Click += new System.EventHandler(this.play_Click);
             //
+            // label1
+            //
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 219);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(645, 39);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "To update the mod, simply run this installer again.";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            //
+            // pictureBox1
+            //
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(273, 294);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(112, 112);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.WaitOnLoad = true;
+            //
             // Finished
             //
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.play);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -85,8 +115,13 @@ namespace psp_papers_installer {
             this.MinimumSize = new System.Drawing.Size(685, 625);
             this.Name = "Finished";
             this.Size = new System.Drawing.Size(685, 625);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
         }
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
 
         private System.Windows.Forms.Button play;
 
