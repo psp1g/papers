bdac3cb5c891c3070c7780e300b7c50e79fcab07
Ethan Jones <ethan@yasfu.net>
2024-04-09 04:19:30 -0400

    feat: give key

--- psp-papers-mod/src/Patches/BoothEnvPatch.cs
+++ psp-papers-mod/src/Patches/BoothEnvPatch.cs
@@ -2,17 +2,26 @@ using System.Linq;
 using HarmonyLib;
 using Il2CppSystem;
 using play.day;
+using test.auto;
 
 namespace psp_papers_mod.Patches;
 
 [HarmonyPatch(typeof(BoothEnv))]
 public class BoothEnvPatch {
 
+    internal static BoothEnv BoothEnv;
+
     private static readonly string[] BlockPaperIDs = ["EntryPermit"];
+
+    [HarmonyPostfix]
+    [HarmonyPatch("__hx_ctor_play_day_BoothEnv", typeof(BoothEnv), typeof(BoothEnvRun), typeof(Day))]
+    static void CtorPostfix(object[] __args) {
+        BoothEnvPatch.BoothEnv = (BoothEnv)__args[0];
+    }
 
     [HarmonyPrefix]
     [HarmonyPatch("addPaper", typeof(string))]
-    private static bool AddPaperPrefix(ref string paperId) {
+    static bool AddPaperPrefix(ref string paperId) {
         Console.Out.WriteLine("PAPER ID " + paperId);
         bool moddedPaper = paperId.StartsWith("modded-");
         if (moddedPaper) paperId = paperId.Replace("modded-", "");
--- psp-papers-mod/src/Patches/GameTransitionPatch.cs
+++ psp-papers-mod/src/Patches/GameTransitionPatch.cs
@@ -11,6 +11,8 @@ namespace psp_papers_mod.Patches {
         [HarmonyPatch(typeof(GameTransition), "call")]
         public static void Prefix(GameTransitionKind gameTransitionKind) {
             Console.Out.WriteLine("KIND: " + gameTransitionKind.toString());
+            if (gameTransitionKind.toString().StartsWith("FADE_TO_ENDLESS_DAY"))
+                BoothEnvPatch.BoothEnv.addPaper(BorderPatch.Border.killRifleButton.keyDeskItemId);
         }
 
     }
