d348aaafe7f2a6c8cee8205923af64b9cf4d37c7
ByteZ1337 <0xbytez@gmail.com>
2024-06-16 22:12:54 +0200

    prevent day from ending in endless mode

--- psp-papers-mod/src/Patches/BorderPatch.cs
+++ psp-papers-mod/src/Patches/BorderPatch.cs
@@ -5,12 +5,23 @@ namespace psp_papers_mod.Patches;
 
 [HarmonyPatch(typeof(Border))]
 public static class BorderPatch {
-
     public static Border Border;
 
     //     BorderPatch.Border.set_snipingEnabled(true); // Not needed?
     //     BorderPatch.Border.sendRunner(); // Works, but turns the endless game into story mode
     //     Console.Out.WriteLine("DESK ITEM ID " + BorderPatch.Border.killRifleButton.keyDeskItemId);
     //     AutoStepTraveler.Give(BorderPatch.Border.killRifleButton.keyDeskItemId); // Doesnt work
+
+    [HarmonyPrefix]
+    [HarmonyPatch("panic")]
+    private static bool AvoidPanic() {
+        return false;
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch("panicLeavingPersonRight")]
+    private static bool AvoidSinglePanic() {
+        return false;
+    }
 
 }
---
+++ psp-papers-mod/src/Patches/StaterPatch.cs
@@ -0,0 +1,21 @@
+using app;
+using HarmonyLib;
+using play.day;
+using play.day.border;
+using System;
+
+namespace psp_papers_mod.Patches;
+
+[HarmonyPatch(typeof(Stater))]
+public static class StaterPatch {
+    [HarmonyPrefix]
+    [HarmonyPatch("go")]
+    private static bool PreventFade(string name, object instant) {
+        Border border = BorderPatch.Border; 
+        if (border is { day.endless: not null }) {
+            return name != "waiting-to-fade-to-night";
+        }
+
+        return true;
+    }
+}
