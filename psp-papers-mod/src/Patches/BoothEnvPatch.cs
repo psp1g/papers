b6db2151830194af959c6bd42b56ffdc299b31b4
LittleBigBug <ethan@yasfu.net>
2024-07-18 16:37:47 -0400

    fix: block work permit

--- psp-papers-mod/src/Patches/BoothEnvPatch.cs
+++ psp-papers-mod/src/Patches/BoothEnvPatch.cs
@@ -15,7 +15,7 @@ public class BoothEnvPatch {
 
     internal static BoothEnv BoothEnv;
 
-    private static readonly string[] BlockPaperIDs = ["EntryPermit", "IdSupplement", "IdCard", "DiplomaticAuth"];
+    private static readonly string[] BlockPaperIDs = ["EntryPermit", "WorkPermit", "IdSupplement", "IdCard", "DiplomaticAuth"];
 
     [HarmonyPostfix]
     [HarmonyPatch("__hx_ctor_play_day_BoothEnv", typeof(BoothEnv), typeof(BoothEnvRun), typeof(Day))]
