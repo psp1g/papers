806aef6180d9f89afe24f7d0809d2868a6cb009f
LittleBigBug <ethan@yasfu.net>
2024-07-18 22:29:22 -0400

    fix: better place to add endless unlocked

--- psp-papers-mod/src/Patches/SettingsPatch.cs
+++ psp-papers-mod/src/Patches/SettingsPatch.cs
@@ -12,6 +12,7 @@ public class SettingsPatch {
     [HarmonyPatch("load")]
     private static void SetTextPrefix(Settings __instance) {
         SettingsPatch.instance = __instance;
+        __instance.set_endlessUnlocked(true);
     }
 
 }
