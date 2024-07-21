a3e9e8da2d179ec3e653f6bc82b7e5eb5e923709
LittleBigBug <ethan@yasfu.net>
2024-07-20 23:57:00 -0400

    fix: remove active chatter after attack

--- psp-papers-mod/src/Patches/StaterPatch.cs
+++ psp-papers-mod/src/Patches/StaterPatch.cs
@@ -1,21 +1,23 @@
 using app;
 using HarmonyLib;
-using play.day;
 using play.day.border;
-using System;
+using psp_papers_mod.Twitch;
 
 namespace psp_papers_mod.Patches;
 
 [HarmonyPatch(typeof(Stater))]
 public static class StaterPatch {
+
     [HarmonyPrefix]
     [HarmonyPatch("go")]
     private static bool PreventFade(string name, object instant) {
-        Border border = BorderPatch.Border; 
-        if (border is { day.endless: not null }) {
-            return name != "waiting-to-fade-to-night" || BorderPatch.ThrewGrenade;
-        }
+        Border border = BorderPatch.Border;
 
-        return true;
+        if (border is not { day.endless: not null }) return true;
+        if (name != "waiting-to-fade-to-night") return true;
+
+        if (BorderPatch.ThrewGrenade) TwitchIntegration.ActiveChatter = null;
+        return BorderPatch.ThrewGrenade;
     }
+
 }
