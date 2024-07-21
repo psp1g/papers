15d3633fc56f44a2fbd2d35cdaedcd081578fecb
ByteZ1337 <0xbytez@gmail.com>
2024-07-21 23:06:33 +0200

    feat: leave command

--- psp-papers-mod/src/Twitch/Commands/Commands.cs
+++ psp-papers-mod/src/Twitch/Commands/Commands.cs
@@ -1,13 +1,24 @@
 using TwitchLib.Client.Models;
+using data;
+using Il2CppSystem;
+using psp_papers_mod.MonoBehaviour;
+using psp_papers_mod.Patches;
+using psp_papers_mod.Utils;
 
 namespace psp_papers_mod.Twitch.Commands;
 
 public static class Commands {
-
     [ChatCommand("leave")]
     public static void LeaveCommand(Chatter sender, ChatMessage chatMessage, string[] _) {
         if (!sender.Moderator && !sender.Streamer && !sender.IsActiveChatter) return;
         // Force the active booth chatter to leave
+        UnityThreadInvoker.Invoke(() => {
+            if (BorderPatch.Border.booth.stater.curState.name != "working") return;
+            BoothEnginePatch.BoothEngine.applyOps(
+                Il2CppUtils.HaxeArrayOf(new Op_ENABLEBUTTON("Leave")),
+                true.ToIl2CppBoxed()
+            );
+        });
     }
 
     [ChatCommand("force")]
@@ -15,7 +26,7 @@ public static class Commands {
         if (!sender.Moderator && !sender.Streamer) return;
 
         Chatter forceChatter = sender;
-        
+
         if (args.Length > 0) {
             string chatterName = args[0].ToLower();
             forceChatter = await PapersPSP.Twitch.FrequentChatters.FromUsername(chatterName);
@@ -25,8 +36,7 @@ public static class Commands {
                 return;
             }
         }
-        
+
         TwitchIntegration.ForcedActiveQueue.Enqueue(forceChatter);
     }
-
 }
---
+++ psp-papers-mod/src/Utils/Il2CppUtils.cs
@@ -0,0 +1,43 @@
+using Il2CppInterop.Runtime.InteropTypes.Arrays;
+using System;
+
+namespace psp_papers_mod.Utils;
+
+public static class Il2CppUtils {
+    public static Array NewHaxeArray(uint size) {
+        return new Array(new Il2CppReferenceArray<Il2CppSystem.Object>(size));
+    }
+
+    public static Array HaxeArrayOf<T>(params T[] elements) where T : Il2CppSystem.Object {
+        Array array = new Array(new Il2CppReferenceArray<Il2CppSystem.Object>(elements.Length));
+        for (int i = 0; i < elements.Length; i++) {
+            array.__set(i, elements[i]);
+        }
+
+        return array;
+    }
+
+    public static Il2CppSystem.Object ToIl2CppBoxed(this bool boolean) {
+        return new Il2CppSystem.Boolean { m_value = boolean }.BoxIl2CppObject();
+    }
+    
+    public static Il2CppSystem.Object ToIl2CppBoxed(this short num) {
+        return new Il2CppSystem.Int16 { m_value = num }.BoxIl2CppObject();
+    }
+
+    public static Il2CppSystem.Object ToIl2CppBoxed(this int num) {
+        return new Il2CppSystem.Int32 { m_value = num }.BoxIl2CppObject();
+    }
+
+    public static Il2CppSystem.Object ToIl2CppBoxed(this long num) {
+        return new Il2CppSystem.Int64 { m_value = num }.BoxIl2CppObject();
+    }
+
+    public static Il2CppSystem.Object ToIl2CppBoxed(this float num) {
+        return new Il2CppSystem.Single { m_value = num }.BoxIl2CppObject();
+    }
+
+    public static Il2CppSystem.Object ToIl2CppBoxed(this double num) {
+        return new Il2CppSystem.Double{ m_value = num }.BoxIl2CppObject();
+    }
+}
